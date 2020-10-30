0bd5bd1d6de07dcff4ca66106621180f281937fd
Valdemir Lima <val7lima@hotmail.com>
2020-10-30 15:18:36 -0300

    alteracao3

--- projetofinal/src/main/java/br/com/projetofinal/controller/MusicaController.java
+++ projetofinal/src/main/java/br/com/projetofinal/controller/MusicaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,26 @@ public class MusicaController {
 
 	@Autowired
 	private MusicaDAO dao;
+	
+	//aqui posso fazer a parte date -- alterando o int para DATE, mas nao seria uma lista
+	@GetMapping("/lancamento/{valor}")
+	public ResponseEntity<List<Musica>> getLancamento(@PathVariable int valor){
+		List<Musica> lista = (List<Musica>) dao.findByLancamento(valor);
+		
+		if(lista.size()==0) {
+			return ResponseEntity.status(404).build();
+			
+		}
+		
+		return ResponseEntity.ok(lista);	
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 	@PostMapping("/novamusica")
 	public ResponseEntity<Musica>  add(@RequestBody Musica objeto){
--- projetofinal/src/main/java/br/com/projetofinal/dao/MusicaDAO.java
+++ projetofinal/src/main/java/br/com/projetofinal/dao/MusicaDAO.java
@@ -1,9 +1,13 @@
 package br.com.projetofinal.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import br.com.projetofinal.bears.Musica;
 
 public interface MusicaDAO extends CrudRepository<Musica, Integer> {
 
+	List<Musica> findByLancamento(int lancamento);
+	// pode fazer a data por aqui, pesquisa de data com esse opção
 }
